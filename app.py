0c2da3fbddfefe4699f51eb00b6e05f28e39bdb1
neginparsa <nickparsa@gmail.com>
2018-03-17 11:42:51 -0700

    Update app.py
    changing app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template,request,session, redirect, url_for, request
+from flask import Flask, jsonify, render_template,request,session
 import googlemaps
 import requests
 import json
@@ -6,34 +6,14 @@ import pprint
 import os
 app = Flask(__name__)
 app.secret_key = "super secret key"
+app.url_map.strict_slashes = False
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method == "POST":
         movie = request.form["movie_id"]
         session["movie_id"] = movie
-        return redirect(url_for('search', movie_id=movie))
+        return render_template('index.html',movie_id=movie)
     return render_template('index.html')
-
-@app.route("/summary")
-def summary():
-    return render_template("index.html")
-
-@app.route("/process")
-def process():
-    return render_template("Process.html")
-
-@app.route("/analytics")
-def analytics():
-    return render_template("Analytics.html")
-
-@app.route("/explore")
-def explore():
-    return render_template("Explore.html")
-
-@app.route("/search")
-def search():
-    return render_template("Search.html")
-
 @app.route("/movie")
 def about():
         
@@ -49,19 +29,20 @@ def about():
     
     api_key='AIzaSyAfjr3okyZP2lyKVPQZ3tcnEaok80qQu9E'
     gm=googlemaps.Client(key=api_key)
-    geocode_result=gm.geocode(address)
-    location=geocode_result[0]['geometry']['location']
+   
+    latlong=[]
+    gm=googlemaps.Client(key=api_key)
+    addlist=[]
+    for el in data_dic['data']['movies'][0]['filmingLocations']:
+        addlist.append(gm.geocode(el['location']))
+    for a in addlist:
+        for b in a:
+            latlong.append((b['geometry']['location']))
     genres=data_dic['data']['movies'][0]['genres']
     urlPoster=data_dic['data']['movies'][0]['urlPoster']
-    parameters=[title,address,releaseDate,year,location,genres,urlPoster]
-    print("WORKS!!!")
-    print(parameters)
-    print(jsonify(parameters))
-    print("DONE!!!")
+    parameters=[title,address,releaseDate,year,latlong,genres,urlPoster]
     return jsonify(parameters)
 
 # 4. Define main behavior
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
